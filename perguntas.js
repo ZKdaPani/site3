7cbd901403f6b6a18b509ac39e90bad376dc6c50
ZKdaPani <160140866+ZKdaPani@users.noreply.github.com>
2024-10-29 19:04:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'velozes e furiosos 1',
+    'Qual é o carro que bryan oconnor usa no filme?',
+    'TOYOTA SUPRA MK4'
+)
+
+criaCartao(
+    'velozes e furiosos 2',
+    'quem é o parceiro de Bryan Oconnor em velozes e furiosos 2?',
+    'Roman Pearce'
+)
+
+criaCartao(
+    'Velozes e Furiosos 3',
+    'Qual é o carro de DK (Drift King) em Velozes e Furiosos 3?',
+    'NISSAN 350Z'
+)
+
+criaCartao(
+    'Velozes e Furiosos 9',
+    'Qual é o nome do personagem que retorna após sua suposta morte?',
+    'Han Lue'
+)
